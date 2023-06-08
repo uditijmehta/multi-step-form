7b7e834fe152af68165bacc1f4403baaad5279e9
Uditi Mehta <mehta.ud@northeastern.edu>
2023-06-08 12:53:20 -0400

    Ignore test files in build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: /\.test.js$/,
+      loader: 'ignore-loader'
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
+
